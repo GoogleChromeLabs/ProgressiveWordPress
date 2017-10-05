cf9c463576758935a955052ca3c5d4e38c46d15c
valdrinkoshi <valdrin@google.com>
2017-10-04 18:33:41 -0700

    restore scroll right before animating in new view

--- src/theme/scripts/router.js
+++ src/theme/scripts/router.js
@@ -101,9 +101,6 @@ class Router {
     const headerAnimation = this._animateHeader(targetIsSingle);
     const newView = this._loadFragment(link);
     const continueAnimation = (await Promise.all([viewAnimation, headerAnimation]))[1];
-    document.scrollingElement.scrollTop = scrollTop;
-    // Set to auto in case user hits page refresh.
-    history.scrollRestoration = 'auto';
     const globalSpinner = await this._globalSpinner;
     if(await Promise.race([newView.ready, timeoutPromise(500)]) === 'timeout')
       globalSpinner.ready.then(_ => {
@@ -112,6 +109,9 @@ class Router {
     await newView.ready;
     globalSpinner.ready.then(_ => globalSpinner.hide());
     oldView.parentNode.replaceChild(newView, oldView);
+    document.scrollingElement.scrollTop = scrollTop;
+    // Set to auto in case user hits page refresh.
+    history.scrollRestoration = 'auto';
     await Promise.all([this._animateIn(newView), continueAnimation()]);
     _pubsubhub.dispatch('navigation', {address: link});
   }
